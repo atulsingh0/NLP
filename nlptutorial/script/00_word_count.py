169a00ee6688955ca3b1c06a887077147fcfd0f7
Atul Kumar Singh <atul.singh@irissoftware.com>
2018-01-30 16:49:58 +0530

    c
    c

--- nlptutorial/script/00_word_count.py
+++ nlptutorial/script/00_word_count.py
@@ -8,17 +8,25 @@ Word Count
 import sys
 
 
-fh = open(sys.argv[1], 'r')
+fh = open(sys.argv[1], 'r', encoding='utf-8')
 
 # creating an ampthy dictionary
 word_count=dict()
 
 for line in fh:
-    words = line.split()
+    words = line.lower().split()
     for word in words:
         word_count[word] = word_count.get(word, 0) + 1
         
-print(word_count)
+#print(word_count)
+n = len(word_count.keys())
+print("No of unique words: ",n)
+print("First few word frequency:" )
+
+if n > 10:
+    print({k :  word_count[k] for k in list(word_count)[:10]})    
+else:
+    print({k :  word_count[k] for k in list(word_count)[:n]})    
 
 # close the file handler
 fh.close()
